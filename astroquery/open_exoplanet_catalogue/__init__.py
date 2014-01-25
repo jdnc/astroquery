d16cccf5f8471c87087624d5acd0e6013922e626
Madhura <madhuraparikh@gmail.com>
2014-01-24 19:54:31 -0600

    moved author and details to init

--- astroquery/open_exoplanet_catalogue/__init__.py
+++ astroquery/open_exoplanet_catalogue/__init__.py
@@ -1,12 +1,22 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-Access to the Open Exoplanet Catalogue.
-Hanno Rein 2013
+Query functions for the Open Exoplanet Catalogue 
+------------------------------------------------
 
-https://github.com/hannorein/open_exoplanet_catalogue
-https://github.com/hannorein/oec_meta
-http://openexoplanetcatalogue.com
+:author: Hanno Rein 2013 (hanno@hanno-rein.de)
+
+External Links
+--------------
+* https://github.com/hannorein/open_exoplanet_catalogue
+* https://github.com/hannorein/oec_meta
+* http://openexoplanetcatalogue.com
+
+Description
+-----------
+The function query_system_xml simply returns the xml file from one of the servers and returns it. 
+The function query_planet searches for a planet and returns its properties as a python dictionary.
 """
+
 from astropy.config import ConfigurationItem
 
 OEC_SERVER = ConfigurationItem('open_exoplanet_catalogue_server', ['https://raw.github.com/hannorein/open_exoplanet_catalogue/master/','http://www.openexoplanetcatalogue.com/open_exoplanet_catalogue/'],'URL of Open Exoplanet Catalogue repository.')
