ceb4940517871fa7d91d1aa92b47e612beab2cb8
Madhura Parikh <madhuraparikh@gmail.com>
2014-01-24 20:41:40 -0600

    split longer lines in init

--- astroquery/open_exoplanet_catalogue/__init__.py
+++ astroquery/open_exoplanet_catalogue/__init__.py
@@ -1,9 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-Query functions for the Open Exoplanet Catalogue 
+Query functions for the Open Exoplanet Catalogue
 ------------------------------------------------
 
-:author: Hanno Rein 2013 (hanno@hanno-rein.de)
+:Author: Hanno Rein (hanno@hanno-rein.de)
 
 External Links
 --------------
@@ -13,13 +13,21 @@ External Links
 
 Description
 -----------
-The function query_system_xml simply returns the xml file from one of the servers and returns it. 
-The function query_planet searches for a planet and returns its properties as a python dictionary.
+The function query_system_xml simply returns the xml file from one of the
+servers and returns it.
+The function query_planet searches for a planet and returns its properties as
+a python dictionary.
 """
 
 from astropy.config import ConfigurationItem
 
-OEC_SERVER = ConfigurationItem('open_exoplanet_catalogue_server', ['https://raw.github.com/hannorein/open_exoplanet_catalogue/master/','http://www.openexoplanetcatalogue.com/open_exoplanet_catalogue/'],'URL of Open Exoplanet Catalogue repository.')
-OEC_META_SERVER = ConfigurationItem('open_exoplanet_catalogue_meta_server', ['https://raw.github.com/hannorein/oec_meta/master/','http://www.openexoplanetcatalogue.com/oec_meta/'],'URL of Open Exoplanet Catalogue repository.')
+OEC_SERVER = ConfigurationItem('open_exoplanet_catalogue_server',
+                               ['https://raw.github.com/hannorein/open_exoplanet_catalogue/master/',
+                                'http://www.openexoplanetcatalogue.com/open_exoplanet_catalogue/'],
+                               'URL of Open Exoplanet Catalogue repository.')
+OEC_META_SERVER = ConfigurationItem('open_exoplanet_catalogue_meta_server',
+                                    ['https://raw.github.com/hannorein/oec_meta/master/',
+                                     'http://www.openexoplanetcatalogue.com/oec_meta/'],
+                                    'URL of Open Exoplanet Catalogue repository.')
 
 from .oec_query import *
